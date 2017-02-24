2244cdf5382fc7f755cc1ce35f8a7a0965ca3727
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-02-24 20:40:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,3 @@
-/*
-4-bit data interface, with busy flag implemented.
-Any LCD pin can be connected to any available I/O port.
-Includes a simple write string routine.
-*/
 #include "LCD_Config.h"
 int main(void){
     LCD_Init(&PIND, &PIND, 0, 1, 2, 3, 4, 5, 6);
